84acb6c38156822cc174765f48cabaf857df8f7d
JannadeV <s1124226@student.hsleiden.nl>
2024-06-12 13:23:19 +0200

    header

---
+++ lib/components/header.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../pages/profiel.dart';
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  const Header({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(100);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return AppBar(
+      title: const Text('Hike Hero'),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.settings_rounded),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Profiel();
+              },
+            ));
+          },
+        ),
+      ],
+    );
+
+  }
+}
