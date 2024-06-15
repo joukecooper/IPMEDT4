9f1792a1d2615695456e54c075fd6cfda1fce7c2
JannadeV <s1124226@student.hsleiden.nl>
2024-06-15 11:44:35 +0200

    More space for actual content; settings better clickable for accessibility

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -5,7 +5,7 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
   const Header({super.key});
 
   @override
-  Size get preferredSize => const Size.fromHeight(100);
+  Size get preferredSize => const Size.fromHeight(80);
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +14,10 @@ class Header extends StatelessWidget implements PreferredSizeWidget {
       title: const Text('Hike Hero'),
       actions: <Widget>[
         IconButton(
-          icon: const Icon(Icons.settings_rounded),
+          icon: const Icon(
+            size: 40,
+            Icons.settings_rounded
+          ),
           onPressed: () {
             Navigator.push(context, MaterialPageRoute<void>(
               builder: (BuildContext context) {
