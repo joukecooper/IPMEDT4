ec997dcfc975421c80dbfc1550e26499475ca940
joukecooper <93555667+joukecooper@users.noreply.github.com>
2024-06-19 13:33:39 +0200

    currentuserid compatible gemaakt met header voor janna

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,33 +1,56 @@
 import 'package:flutter/material.dart';
 import '../pages/account_page.dart';
+import '../services/auth_service.dart';
 
 class Header extends StatelessWidget implements PreferredSizeWidget {
-  const Header({super.key});
+  const Header({Key? key}) : super(key: key);
 
   @override
   Size get preferredSize => const Size.fromHeight(80);
 
   @override
   Widget build(BuildContext context) {
+    String? currentUserID;
 
-    return AppBar(
-      title: const Text('Hike Hero'),
-      actions: <Widget>[
-        IconButton(
-          icon: const Icon(
-            size: 40,
-            Icons.settings_rounded
-          ),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute<void>(
-              builder: (BuildContext context) {
-                return const AccountPage();
+    Future<void> getCurrentUserID() async {
+      final authService = AuthService();
+      final uid = await authService.getCurrentUID();
+      currentUserID = uid;
+      (context as Element).markNeedsBuild();
+    }
+
+    getCurrentUserID();
+
+    return FutureBuilder(
+      future: getCurrentUserID(),
+      builder: (context, snapshot) {
+        return AppBar(
+          title: const Text('Hike Hero'),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(
+                Icons.settings_rounded,
+                size: 40,
+              ),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute<void>(
+                  builder: (BuildContext context) {
+                    return AccountPage();
+                  },
+                ));
               },
-            ));
-          },
-        ),
-      ],
+            ),
+            if (currentUserID != null)
+              IconButton(
+                icon: const Icon(Icons.person),
+                onPressed: () {
+                  // Debugging voor testen of juist is gegaan
+                  print('Current User ID: $currentUserID');
+                },
+              ),
+          ],
+        );
+      },
     );
-
   }
 }
